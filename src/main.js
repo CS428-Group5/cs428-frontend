9ad517dd23a88dc01c0b44ef67a4aaae71e82049
hoai1107 <khaihoai1107@gmail.com>
2023-06-28 22:02:51 +0700

    Add css files

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/reset.css'
 import '@/assets/base.css'
+import '@/assets/styles.css'
 
 const app = createApp(App)
 
