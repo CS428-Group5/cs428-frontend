97edc54b51ca7d83f22c5366f4c1690fd8f826f5
Trong <tvdtrong@gmail.com>
2023-07-14 15:41:29 +0700

    add 5 pages, session, history, fav, pay, confirm

---
+++ src/views/Favmentor.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="text-3xl font-bold mb-12">My favorite mentors</h1>
+    <div class="grid grid-cols-12 gap-x-6 gap-y-8">
+        <div v-for="n in 9" :key="n" class="col-span-3">
+            <MentorItem />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import MentorItem from './MentorListView/MentorItem.vue';
+</script>
