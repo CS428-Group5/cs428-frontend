886b3b9684d2304c1f5554c8d5ce90fc42fa9640
Nhu-Huynh <mannhuch@gmail.com>
2023-07-19 09:16:05 +0700

    mentor details

--- src/views/MentorDetailsView/MentorDetailsView.vue
+++ src/views/MentorDetailsView/MentorDetailsView.vue
@@ -26,66 +26,71 @@ import MentorInfo from './MentorInfo.vue'
 import MentorReviews from './MentorReviews.vue'
 import avatar from '@/assets/avatar.jpg'
 
-import axios from 'axios'
+import client from '../../axios/client.ts'
 import { useRoute } from 'vue-router'
 
 const router = useRoute()
-console.log(router.params);
-
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "X-CSRFToken";
-axios.defaults.withCredentials = true;
-
-await axios
-  .get(
-    `http://127.0.0.1:8000/api/mentors/${router.params.id}`, 
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
-      }
-    }
-  )
-  .then(res => console.log(res.data))
-  .catch(e => console.log('error', e))
-
-const sampleMentor = {
-  name: 'Darlene Robertson',
-  work: 'Software Engineer at Microsoft',
-  price: 50000,
-  remainingSession: 2,
-  expertise: 'Engineer',
-  experience: 2,
-  about:
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.\
-   Facilis perspiciatis a, velit tenetur ab distinctio optio earum quae atque maiores, dolor fugiat eligendi laudantium?\
-   Repellendus ullam temporibus eaque asperiores quaerat.'
+const mentor = {
+  name: '',
+  work: '',
+  price: 0,
+  remainingSession: 0,
+  expertise: '',
+  experience: 0,
+  about: ''
 }
-
 const reviews = [
   {
-    author: 'Kathryn Murphy',
-    avatar: avatar,
-    comment:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti excepturi corporis, hic voluptatem qui ullam, repellat ipsum suscipit veritatis accusamus libero aut unde animi dolor aperiam accusantium vitae deserunt quae.',
-    score: 5
-  },
-  {
-    author: 'Jacob Jones',
-    avatar: avatar,
-    comment:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti excepturi corporis, hic voluptatem qui ullam, repellat ipsum suscipit veritatis accusamus libero aut unde animi dolor aperiam accusantium vitae deserunt quae.',
-    score: 4
-  },
-  {
-    author: 'Savannah Nguyen',
-    avatar: avatar,
-    comment:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti excepturi corporis, hic voluptatem qui ullam, repellat ipsum suscipit veritatis accusamus libero aut unde animi dolor aperiam accusantium vitae deserunt quae.',
-    score: 5
+    author: '',
+    avatar: '',
+    comment: '',
+    score: 0
   }
 ]
+const error = ''
+
+console.log(router.params.id)
+await client
+  .get(`http://127.0.0.1:8000/api/mentors/${router.params.id}`)
+  .then((res) => console.log(res.data))
+  .catch((e) => console.log('error,', e))
+
+// const sampleMentor = {
+//   name: 'Darlene Robertson',
+//   work: 'Software Engineer at Microsoft',
+//   price: 50000,
+//   remainingSession: 2,
+//   expertise: 'Engineer',
+//   experience: 2,
+//   about:
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.\
+//    Facilis perspiciatis a, velit tenetur ab distinctio optio earum quae atque maiores, dolor fugiat eligendi laudantium?\
+//    Repellendus ullam temporibus eaque asperiores quaerat.'
+// }
+
+// const reviews = [
+//   {
+//     author: 'Kathryn Murphy',
+//     avatar: avatar,
+//     comment:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti excepturi corporis, hic voluptatem qui ullam, repellat ipsum suscipit veritatis accusamus libero aut unde animi dolor aperiam accusantium vitae deserunt quae.',
+//     score: 5
+//   },
+//   {
+//     author: 'Jacob Jones',
+//     avatar: avatar,
+//     comment:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti excepturi corporis, hic voluptatem qui ullam, repellat ipsum suscipit veritatis accusamus libero aut unde animi dolor aperiam accusantium vitae deserunt quae.',
+//     score: 4
+//   },
+//   {
+//     author: 'Savannah Nguyen',
+//     avatar: avatar,
+//     comment:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti excepturi corporis, hic voluptatem qui ullam, repellat ipsum suscipit veritatis accusamus libero aut unde animi dolor aperiam accusantium vitae deserunt quae.',
+//     score: 5
+//   }
+// ]
 </script>
 
 <style lang="scss" scoped></style>
