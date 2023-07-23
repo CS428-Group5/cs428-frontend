7265282235cb322e96674608b07b24e7f46c1d56
hoai1107 <khaihoai1107@gmail.com>
2023-07-23 23:20:04 +0700

    Integrate backend to frontend

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "X-CSRFToken";
-axios.defaults.withCredentials = true;
+// axios.defaults.xsrfCookieName = "csrftoken";
+// axios.defaults.xsrfHeaderName = "X-CSRFToken";
+// axios.defaults.withCredentials = true;
+
 const client = axios.create({
-    baseURL: `http://localhost:8000/api`,
+    baseURL: `http://34.29.48.120:8000/api`,
     
 })
 
