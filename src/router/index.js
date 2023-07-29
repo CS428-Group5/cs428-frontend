76c51dec7c8068b89eda762ab9523fb34d516220
Nhu-Huynh <mannhuch@gmail.com>
2023-07-29 17:05:48 +0700

    implement user profile page

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,11 @@ const router = createRouter({
       component: () => import('@/views/MentorDetailsView/MentorDetailsView.vue')
     },
     {
+      path: '/users/:id',
+      name: 'users',
+      component: () => import('@/views/UserProfileView/UserProfileView.vue')
+    },
+    {
       path: '/login',
       name: 'login',
       component: () => import('@/views/LoginView/LoginView.vue')
