53498eb974ed24b1064d9ebafd68dcdef342722e
Nhu-Huynh <mannhuch@gmail.com>
2023-07-31 21:00:08 +0700

    add calendar to session

---
+++ src/views/SessionManagement/ChoosingSession.vue
@@ -0,0 +1,30 @@
+<template>
+    <VueDatePicker 
+        v-model="date" 
+        :enable-time-picker="false"
+        auto-apply 
+        :close-on-auto-apply="false"
+        :allowed-dates="allowedDates"
+    >
+    </VueDatePicker>
+</template>
+
+<script setup>
+import { ref, watchEffect, computed } from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
+const date = ref(new Date());
+watchEffect(() => console.log(date.value))
+
+const allowedDates = computed(() => {
+  return [
+    new Date(),
+    new Date(new Date().setDate(new Date().getDate() + 1)),
+    "Tue Aug 03 2023 15:50:00 GMT+0700 (Indochina Time)"
+  ];
+});
+
+</script>
+
+<style lang="scss" scoped></style>
