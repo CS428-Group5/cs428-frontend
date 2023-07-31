1d219b5fafd32a171d77ee08f2c08d864e8f307f
nhu.huynh <139065350+Nhu-Huynhh@users.noreply.github.com>
2023-07-31 16:37:49 +0700

    Revert "add datepicker to session"
    This reverts commit f1acf00feb96169a3e07ca611fc959e956d97184.

--- src/views/SessionManagement/SessionList.vue
+++ src/views/SessionManagement/SessionList.vue
@@ -2,7 +2,6 @@
     <div class="grid grid-cols-12 gap-x-6">
         <div class="col-start-3 col-span-8 ">
             <h1 class="text-3xl font-bold mb-4 text-center">Session Management</h1>
-            <ChoosingSession></ChoosingSession>
             <div v-for="n in 9" :key="n">
                 <SessionItem></SessionItem>
             </div>
@@ -12,5 +11,4 @@
 
 <script setup>
 import SessionItem from './SessionItem.vue';
-import ChoosingSession from './ChoosingSession.vue'
 </script>
