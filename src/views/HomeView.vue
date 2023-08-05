e02c3ad1ba3de6447f00145f1d0cf43e49b98a1f
hoai1107 <khaihoai1107@gmail.com>
2023-08-05 13:37:50 +0700

    Minor update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,12 @@
     <div class="col-span-6">
       <p class="text-xl font-bold mb-4">Don’t know which career to go?</p>
       <p class="text-3xl font-bold mb-6">Let Mentoree help you!</p>
-      <a href="#" type="button" class="bg-blue w-fit px-4 py-3 text-white rounded-2xl">
+      <a href="/mentors" type="button" class="bg-blue w-fit px-4 py-3 text-white rounded-2xl">
         See all Mentors
       </a>
     </div>
     <div class="col-span-6">
-      <img src="../assets/background.png" alt="mentors">
+      <img src="../assets/background.png" alt="mentors" />
     </div>
   </div>
 </template>
