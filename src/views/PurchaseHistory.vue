d8476ab9f11d3da112f9926e8310bf200ef0e315
Thai Vo Duc Trong <70931622+dtrongtvo@users.noreply.github.com>
2023-08-09 22:16:56 +0700

    add purchase history for mentor

--- src/views/PurchaseHistory.vue
+++ src/views/PurchaseHistory.vue
@@ -2,13 +2,19 @@
   <div class="grid grid-cols-12 gap-x-6">
     <div class="col-start-3 col-span-8">
       <h1 class="text-3xl font-bold mb-4 text-center">Purchase History</h1>
-      <div v-for="n in 3" :key="n">
-        <PurchaseItem></PurchaseItem>
+      <div v-if="isMentee" v-for="n in 3" :key="n">
+        <PurchaseItemMentee>
+        </PurchaseItemMentee>
+      </div>
+      <div v-else v-for="n in 3" :key="n">
+        <PurchaseItemMentor>
+        </PurchaseItemMentor>
       </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import PurchaseItem from './PurchaseItem.vue'
+import PurchaseItemMentee from './PurchaseItemMentee.vue';
+import PurchaseItemMentor from './PurchaseItemMentor.vue';
+const isMentee = ref(true)
 </script>
